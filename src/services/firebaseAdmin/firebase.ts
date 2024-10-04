72a790758331a274ccdbb0a3714a304542b80ae4
Francisco Ossian <ossian.neto@gmail.com>
2024-10-04 14:13:36 -0300

    hotfix (badfix)

--- src/services/firebaseAdmin/firebase.ts
+++ src/services/firebaseAdmin/firebase.ts
@@ -1,5 +1,21 @@
 import admin from "firebase-admin";
-import serviceAccount from "./key.json";
+
+//const jsonString = JSON.stringify(serviceAccount);
+//const base64String = Buffer.from(jsonString).toString("base64");
+//console.log(base64String);
+
+let serviceAccount;
+
+try {
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  serviceAccount = require("./key.json");
+} catch {
+  serviceAccount = JSON.parse(
+    Buffer.from(process.env.FIREBASE_ADMIN_KEY_BASE64 ?? "", "base64").toString(
+      "utf-8"
+    )
+  );
+}
 
 // Inicializar Firebase apenas se não estiver inicializado
 if (!admin.apps.length) {
